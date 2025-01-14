31248f22e0671289b01237cef2453506e899d17a
Sonhong1805 <nguyenhongson18052003@gmail.com>
2025-01-14 12:50:01 +0700

    API Login user

---
+++ src/modules/auth/auth.guard.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get('jwtAuth').jwtTokenSecret,
+      });
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request) {
+    //https://stackoverflow.com/questions/50284841/how-to-extract-token-string-from-bearer-token
+    if (
+      request.headers.authorization &&
+      request.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+      return request.headers.authorization.split(' ')[1];
+    }
+    return null;
+  }
+}
