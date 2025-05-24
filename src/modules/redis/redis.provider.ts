4851412c68c303f516b5b55106ba87aa4bcbbbf8
Sonhong1805 <nguyenhongson18052003@gmail.com>
2025-05-24 08:44:50 +0700

    fixed config redis 4 time

--- src/modules/redis/redis.provider.ts
+++ src/modules/redis/redis.provider.ts
@@ -9,23 +9,8 @@ export const redisProvider: Provider[] = [
     provide: REDIS_CLIENT,
     inject: [ConfigService],
     useFactory: (configService: ConfigService): RedisClient => {
-      const redisUri = configService.get<string>('REDIS_URI');
-
-      const client = new Redis(redisUri, {
-        tls: {},
-        maxRetriesPerRequest: null,
-        enableReadyCheck: true,
-      });
-
-      client.on('connect', () => {
-        console.log('✅ Connected to Upstash Redis');
-      });
-
-      client.on('error', (err) => {
-        console.error('❌ Redis connection error:', err);
-      });
-
-      return client;
+      const REDIS_URI = configService.get('redisUri');
+      return new Redis(REDIS_URI);
     },
   },
 ];
