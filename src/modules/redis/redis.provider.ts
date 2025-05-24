d85ada91155052a93b1eaa0570a83e598dbb9f62
Sonhong1805 <nguyenhongson18052003@gmail.com>
2025-05-24 08:16:08 +0700

    fixed config redis

--- src/modules/redis/redis.provider.ts
+++ src/modules/redis/redis.provider.ts
@@ -10,7 +10,16 @@ export const redisProvider: Provider[] = [
     inject: [ConfigService],
     useFactory: (configService: ConfigService): RedisClient => {
       const REDIS_URI = configService.get('redisUri');
-      return new Redis(REDIS_URI);
+      return new Redis(REDIS_URI, {
+        maxRetriesPerRequest: null, // vô hiệu hóa giới hạn retry
+        enableReadyCheck: false, // tùy chọn: bỏ kiểm tra 'ready'
+        retryStrategy: (times) => {
+          if (times >= 10) {
+            return null; // ngừng retry sau 10 lần
+          }
+          return Math.min(times * 100, 3000); // retry sau thời gian tăng dần
+        },
+      });
     },
   },
 ];
